ee404d7133b12ea903a701589ab88251be41e390
Maximiliano Bloga <maxibdn6@gmail.com>
2026-04-07 12:32:18 -0300

    probando a ver si anda en vercel

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-    res.send('Hola mundo del ejercicio 1!');
+    res.send('Hola mundo del ejercicio 1!!!');
 });
 
 app.listen(PORT, () => {
